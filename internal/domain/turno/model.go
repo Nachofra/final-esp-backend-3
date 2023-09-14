ba13267141308b873a93362a2732bb1662d967d4
Gonzasan <grsac29@gmail.com>
2023-09-14 19:49:18 -0300

    fix RequestTurno name

--- internal/domain/turno/model.go
+++ internal/domain/turno/model.go
@@ -4,17 +4,17 @@ import "time"
 
 // Turno describes a turno.
 type Turno struct {
-	ID             int         `json:"id"`
-	Paciente       paciente    `json:"paciente_id"`
-	Odontologo     odontologo  `json:"odontologo_id"`
-	Date           time.Time   `json:"date"`
-	Descrtiption   string      `json:"description"`
+	ID           int        `json:"id"`
+	Paciente     paciente   `json:"paciente_id"`
+	Odontologo   odontologo `json:"odontologo_id"`
+	Date         time.Time  `json:"date"`
+	Descrtiption string     `json:"description"`
 }
 
 // RequestTurno describes the data needed to create a new turno.
-type RequestPaciente struct {
-	Paciente       paciente    `json:"paciente_id"`
-	Odontologo     odontologo  `json:"odontologo_id"`
-	Date           time.Time   `json:"date"`
-	Descrtiption   string      `json:"description"`
+type RequestTurno struct {
+	Paciente     paciente   `json:"paciente_id"`
+	Odontologo   odontologo `json:"odontologo_id"`
+	Date         time.Time  `json:"date"`
+	Descrtiption string     `json:"description"`
 }
