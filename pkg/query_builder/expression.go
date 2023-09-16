95ef511eaa4b1ac19a2a78a2aa2329c3420426b0
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2023-09-16 19:22:51 -0300

    docs: added missings documentation

--- pkg/query_builder/expression.go
+++ pkg/query_builder/expression.go
@@ -7,12 +7,15 @@ import (
 	"strings"
 )
 
+// Expression represents a portion of a SQL query, example: "id >= 1".
 type Expression struct {
 	Key   string
 	Exp   string
 	Value interface{}
 }
 
+// ToString converts an Expression.Value to a string and formats it for query construction.
+// It ensures proper escaping and returns a string representation of the expression.
 func (e Expression) ToString() string {
 
 	switch e.Value.(type) {
@@ -34,6 +37,8 @@ func (e Expression) ToString() string {
 	}
 }
 
+// getReplaceExp parses type to SQL query format, when type is int we need to be without quotes,
+// and all the others types goes with quotes.
 func (e Expression) getReplaceExp() string {
 	switch e.Value.(type) {
 	case int, int64, int32, int16:
--- pkg/query_builder/query_builder.go
+++ pkg/query_builder/query_builder.go
@@ -5,20 +5,26 @@ import (
 	"strings"
 )
 
+// DynamicQueryBuilder is the core of the package, it wraps a native string type.
 type DynamicQueryBuilder string
 
+// NewExpression creates a new SQL Expression.
 func (dqb DynamicQueryBuilder) NewExpression(key string, assignment string, value interface{}) Expression {
 	return Expression{Key: key, Exp: assignment, Value: value}
 }
 
+// And wraps multiple expressions with the "AND" operator of SQL.
 func (dqb DynamicQueryBuilder) And(component ...interface{}) DynamicQueryBuilder {
 	return dqb.getOperationExpression("AND", component...)
 }
 
+// Or wraps multiple expressions with the "OR" operator of SQL.
 func (dqb DynamicQueryBuilder) Or(component ...interface{}) DynamicQueryBuilder {
 	return dqb.getOperationExpression("OR", component...)
 }
 
+// getOperationExpression generates a DynamicQueryBuilder by combining multiple expressions with the specified SQL operator.
+// Example of SQL operators: "AND", "OR".
 func (dqb DynamicQueryBuilder) getOperationExpression(operation string, component ...interface{}) DynamicQueryBuilder {
 	if len(component) == 0 {
 		return ""
@@ -42,17 +48,22 @@ func (dqb DynamicQueryBuilder) getOperationExpression(operation string, componen
 	}
 }
 
+// Limit sets "LIMIT" and "OFFSET" for the query.
 func (dqb DynamicQueryBuilder) Limit(offset int, length int) DynamicQueryBuilder {
 	query := string(dqb)
 	query += " LIMIT " + strconv.Itoa(length) + " OFFSET " + strconv.Itoa(offset)
 	return DynamicQueryBuilder(query)
 }
 
+// CopyQuery parses a string query into the DynamicQueryBuilder type and stores the result in the provided destination string.
+// It returns the DynamicQueryBuilder for method chaining if required.
 func (dqb DynamicQueryBuilder) CopyQuery(dest *string) DynamicQueryBuilder {
 	*dest = dqb.ToString()
 	return dqb
 }
 
+// BindSql generates and returns the final SQL query by combining the DynamicQueryBuilder with an existing SQL string.
+// Example of existing SQL string: "SELECT * FROM table".
 func (dqb DynamicQueryBuilder) BindSql(sql string) string {
 	if dqb != "" && dqb != "( )" {
 		index := strings.Index(dqb.ToString(), "LIMIT")
@@ -65,10 +76,12 @@ func (dqb DynamicQueryBuilder) BindSql(sql string) string {
 	return sql
 }
 
+// ToString converts the DynamicQueryBuilder to a string representation of the query.
 func (dqb DynamicQueryBuilder) ToString() string {
 	return string(dqb)
 }
 
+// componentToString converts various types of components into a DynamicQueryBuilder for query construction.
 func componentToString(c interface{}) DynamicQueryBuilder {
 	switch v := c.(type) {
 	case Expression:
