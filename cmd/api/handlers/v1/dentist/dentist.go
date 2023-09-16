9b45902b6aa293b3279cde06f3fd28dc8073295c
Gonzasan <grsac29@gmail.com>
2023-09-16 16:50:12 -0300

    added service and handler

--- cmd/api/handlers/v1/dentist/dentist.go
+++ cmd/api/handlers/v1/dentist/dentist.go
@@ -1,13 +1,183 @@
 package dentist
 
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Nachofra/final-esp-backend-3/internal/domain/dentist"
+	"github.com/Nachofra/final-esp-backend-3/pkg/web"
+	"github.com/gin-gonic/gin"
+)
+
 // Here we will implement dentist handlers
 
 type Handler struct {
 	// Here we put the service
+	service dentist.Service
 }
 
-func New() *Handler {
+func NewHandler(service dentist.Service) *Handler {
 	return &Handler{
 		// Receive service via method param
+		service: service,
+	}
+}
+
+// Dentist godoc
+// @Summary dentist example
+// @Description Create a new dentist
+// @Tags dentist
+// @Accept json
+// @Produce json
+// @Success 200 {object} web.response
+// @Failure 400 {object} web.errorResponse
+// @Failure 500 {object} web.errorResponse
+// @Router /dentist [post]
+func (h *Handler) Create() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		var request dentist.NewDentist
+
+		err := ctx.Bind(&request)
+
+		if err != nil {
+			web.Error(ctx, http.StatusBadRequest, "%s", "bad request")
+			return
+		}
+
+		product, err := h.service.Create(ctx, request)
+		if err != nil {
+			web.Error(ctx, http.StatusInternalServerError, "%s", "internal server error")
+			return
+		}
+
+		web.Success(ctx, http.StatusOK, gin.H{
+			"data": product,
+		})
+
+	}
+}
+
+// Dentist godoc
+// @Summary denist example
+// @Description Get all dentists
+// @Tags dentist
+// @Accept json
+// @Produce json
+// @Success 200 {object} web.response
+// @Failure 500 {object} web.errorResponse
+// @Router /dentist [get]
+func (h *Handler) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		dentist := h.service.GetAll(ctx)
+
+		web.Success(ctx, http.StatusOK, gin.H{
+			"data": dentist,
+		})
+	}
+}
+
+// Dentist godoc
+// @Summary dentist example
+// @Description Get dentist by id
+// @Tags dentist
+// @Param id path int true "dentist id"
+// @Accept json
+// @Produce json
+// @Success 200 {object} web.response
+// @Failure 400 {object} web.errorResponse
+// @Failure 500 {object} web.errorResponse
+// @Router /dentist/:id [get]
+func (h *Handler) GetByID() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			web.Error(ctx, http.StatusBadRequest, "%s", "id invalido")
+			return
+		}
+
+		dentist, err := h.service.GetByID(ctx, id)
+		if err != nil {
+			web.Error(ctx, http.StatusInternalServerError, "%s", "internal server error")
+			return
+		}
+
+		web.Success(ctx, http.StatusOK, gin.H{
+			"data": dentist,
+		})
+	}
+}
+
+// Dentist godoc
+// @Summary dentist example
+// @Description Update dentist by id
+// @Tags dentist
+// @Accept json
+// @Produce json
+// @Success 200 {object} web.response
+// @Failure 400 {object} web.errorResponse
+// @Failure 500 {object} web.errorResponse
+// @Router /dentist/:id [put]
+func (h *Handler) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		var request dentist.NewDentist
+		errBind := ctx.Bind(&request)
+
+		if errBind != nil {
+			web.Error(ctx, http.StatusBadRequest, "%s", "bad request binding")
+			return
+		}
+
+		id := ctx.Param("id")
+
+		idInt, err := strconv.Atoi(id)
+
+		if err != nil {
+			web.Error(ctx, http.StatusBadRequest, "%s", "bad request param")
+			return
+		}
+
+		dentist, err := h.service.Update(ctx, request, idInt)
+		if err != nil {
+			web.Error(ctx, http.StatusInternalServerError, "%s", "internal server error")
+			return
+		}
+
+		web.Success(ctx, http.StatusOK, gin.H{
+			"data": dentist,
+		})
+
+	}
+}
+
+// Dentist godoc
+// @Summary dentist example
+// @Description Delete dentist by id
+// @Tags dentist
+// @Param id path int true "dentist id"
+// @Accept json
+// @Produce json
+// @Success 200 {object} web.response
+// @Failure 400 {object} web.errorResponse
+// @Failure 500 {object} web.errorResponse
+// @Router /dentist/:id [delete]
+func (h *Handler) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			web.Error(ctx, http.StatusBadRequest, "%s", "invalid id")
+			return
+		}
+
+		err = h.service.Delete(ctx, id)
+		if err != nil {
+			web.Error(ctx, http.StatusInternalServerError, "%s", "internal server error")
+			return
+		}
+
+		web.Success(ctx, http.StatusOK, gin.H{
+			"mensaje": "dentist deleted",
+		})
 	}
 }
--- internal/domain/dentist/dentist.go
+++ internal/domain/dentist/dentist.go
@@ -3,6 +3,7 @@ package dentist
 import (
 	"context"
 	"errors"
+	"log"
 )
 
 var (
@@ -18,4 +19,86 @@ type Store interface {
 	GetByID(ctx context.Context, id int) (Dentist, error)
 	Update(ctx context.Context, dentist Dentist) (Dentist, error)
 	Delete(ctx context.Context, id int) error
+}
+
+type service struct {
+	store Store
+}
+
+type Service interface {
+	Create(ctx context.Context, newDentist NewDentist) (Dentist, error)
+	GetAll(ctx context.Context) []Dentist
+	GetByID(ctx context.Context, id int) (Dentist, error)
+	Update(ctx context.Context, newDentist NewDentist, id int) (Dentist, error)
+	Delete(ctx context.Context, id int) error
+}
+
+// NewService creates a new product service.
+func NewService(store Store) Service {
+	return &service{
+		//Here we put the repository
+		store: store,
+	}
+}
+
+// Create creates a new product.
+func (s *service) Create(ctx context.Context, newDentist NewDentist) (Dentist, error) {
+	dentist := requestToDentist(newDentist)
+	response, err := s.store.Create(ctx, dentist)
+	if err != nil {
+		log.Println("error en servicio. Metodo create")
+		return Dentist{}, errors.New("error en servicio. Metodo create")
+	}
+
+	return response, nil
+}
+
+// GetAll returns all products.
+func (s *service) GetAll(ctx context.Context) []Dentist {
+	productos := s.store.GetAll(ctx)
+	return productos
+}
+
+// GetByID returns a product by its ID.
+func (s *service) GetByID(ctx context.Context, id int) (Dentist, error) {
+	dentist, err := s.store.GetByID(ctx, id)
+	if err != nil {
+		log.Println("error getting dentist on service layer", err.Error())
+		return Dentist{}, errors.New("service error. Method GetByID")
+	}
+
+	return dentist, nil
+}
+
+// Update updates a product.
+func (s *service) Update(ctx context.Context, newDentist NewDentist, id int) (Dentist, error) {
+	dentist := requestToDentist(newDentist)
+	dentist.ID = id
+	response, err := s.store.Update(ctx, dentist)
+	if err != nil {
+		log.Println("error updating dentist on service layer", err.Error())
+		return Dentist{}, errors.New("service error. Method Update")
+	}
+
+	return response, nil
+}
+
+// Delete deletes a product.
+func (s *service) Delete(ctx context.Context, id int) error {
+	err := s.store.Delete(ctx, id)
+	if err != nil {
+		log.Println("error deleting dentist on service layer", err.Error())
+		return errors.New("service error. Method Delete")
+	}
+
+	return nil
+}
+
+func requestToDentist(newDentist NewDentist) Dentist {
+	var dentist Dentist
+	dentist.FirstName = newDentist.FirstName
+	dentist.LastName = newDentist.LastName
+	dentist.RegistrationNumber = newDentist.RegistrationNumber
+
+	return dentist
 }
