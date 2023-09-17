a330e8519823de9462484192d2ed9841e48aacee
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-09-17 13:22:56 -0300

    Minor change: endpoints with param /:dni

--- cmd/api/handlers/v1/apointment/appointment.go
+++ cmd/api/handlers/v1/apointment/appointment.go
@@ -67,9 +67,10 @@ func (h *Handler) Create() gin.HandlerFunc {
 // @Produce json
 // @Success 200 {object} web.response
 // @Failure 500 {object} web.errorResponse
-// @Router /appointment [get]
+// @Router /appointment/:dni [get]
 func (h *Handler) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		ctx.Param("dni")
 
 		// TODO agregar filtro para buscar por dni de paciente
 		appointment := h.service.GetAll(ctx, appointment.FilterAppointment{})
@@ -200,11 +201,12 @@ func (h *Handler) Delete() gin.HandlerFunc {
 // @Success 200 {object} web.response
 // @Failure 400 {object} web.errorResponse
 // @Failure 500 {object} web.errorResponse
-// @Router /appointment/DNI [post]
+// @Router /appointment/:dni [post]
 func (h *Handler) CreateByDNI() gin.HandlerFunc {
 	// TODO crear metodo para crear turno con dni de paciente
 	return func(ctx *gin.Context) {
 
+		ctx.Param("dni")
 		var request appointment.NewAppointment
 
 		err := ctx.Bind(&request)
