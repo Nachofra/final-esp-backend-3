b69eee6ed188064b416519e2da39729bea256db0
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2023-09-17 16:03:14 -0300

    docs: deletes old comment

--- cmd/api/handlers/v1/v1.go
+++ cmd/api/handlers/v1/v1.go
@@ -6,8 +6,6 @@ import (
 	"log"
 )
 
-// Here we will set all routes of the application and some dependencies
-
 // Config has all the dependencies and requirements to initialize handlers.
 type Config struct {
 	Log log.Logger
