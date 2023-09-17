965addae1b94a71cf9a9a9903419c5730b37c49d
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2023-09-17 16:01:28 -0300

    feat: added routes scaffolding

--- cmd/api/handlers/v1/v1.go
+++ cmd/api/handlers/v1/v1.go
@@ -1,3 +1,33 @@
 package v1
 
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
 // Here we will set all routes of the application and some dependencies
+
+// Config has all the dependencies and requirements to initialize handlers.
+type Config struct {
+	Log log.Logger
+	DB  *sql.DB
+}
+
+// Routes sets all the version 1 routes.
+func Routes(eng *gin.Engine, cfg *Config) {
+	cfg.Log.Println("configuring v1 routes")
+
+	const prefix = "/v1"
+	//v1 := eng.Group(prefix)
+
+	// dentistService := dentist.NewDentist(mysql.New(cfg.DB))
+
+	{
+		//dentistHandler := dentist.NewHandler(dentistService)
+		//dentist := v1.Group("dentist")
+		//
+		//dentist.GET()
+		//etc...
+	}
+}
